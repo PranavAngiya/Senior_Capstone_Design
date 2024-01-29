5d0ca82992a6cf4bbd3fa9e7bd99ddf4f1c37510
Aditya Sharma <adityas0911@gmail.com>
2024-01-29 17:37:22 -0500

    reset

--- HEMMS/App.js
+++ HEMMS/App.js
@@ -1,21 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
-
-import Button from '../components/Button';
-import ImageViewer from '../components/ImageViewer';
-
-const PlaceholderImage = require('./assets/images/background-image.png');
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
+      <Text>This is HEMMS!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -24,20 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#fff',
     alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
